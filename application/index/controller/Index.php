a4a94db18f3d2edc2b89a22048ba283b4ca9fa08
xiaochaozi <29862285@qq.com>
2019-03-14 12:01:16 +0800

    swg

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,8 +4,15 @@ use think\Controller;
 
 class Index extends Controller
 {
-    public function index()
-    {
-         return $this->fetch();
+	public function index(){
+/*        $path = ROOT_PATH.'application\index'; //你想要哪个文件夹下面的注释生成对应的API文档
+        $swagger = \Swagger\scan($path);
+
+        $swagger_json_path = ROOT_PATH.'public\swagger.json';
+        $res = file_put_contents($swagger_json_path, $swagger);*/
+        $cmd = 'php '.ROOT_PATH.'vendor\zircote\swagger-php\bin\swagger '.ROOT_PATH.'application\index -o '.ROOT_PATH.'public';
+
+        $res = shell_exec($cmd);
+        $this->redirect('http://game.com/swagger-ui/dist/index.html');
     }
 }
--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -7,6 +7,41 @@ class Login extends Controller
     public function checkLogin()
     {
          echo "1";
+         
+    }
+    /**
+     * @SWG\Get(path="/index/login/getAuthCode",
+     *   tags={"store"},
+     *   description="获取验证码",
+     *   operationId="getInventory",
+     *   produces={"application/json"},
+     *   parameters={},
+     *   @SWG\Response(
+     *     response=200,
+     *     description="successful operation",
+     *     @SWG\Schema(
+     *       type="object",
+     *       additionalProperties={
+     *         "type":"integer",
+     *         "format":"int32"
+     *       }
+     *     )
+     *   ),
+     *   security={{
+     *     "api_key":{}
+     *   }}
+     * )
+     */
+    public function getAuthCode()
+    {
+    	$captcha = new \think\captcha\Captcha((array)\think\Config::get('captcha'));
+		return $captcha->entry();
+    }
+    public function captcha_check()
+    {
+    	$captcha = $_GET['code'];
+    	$res = captcha_check($captcha);
+    	var_dump($res);
     }
 
 }
