b04e8131b1cccf6cde527e4ecead1874d6460f96
xiaochaozi <29862285@qq.com>
2019-03-21 21:36:49 +0800

    左侧信息接口

--- application/api/controller/Home.php
+++ application/api/controller/Home.php
@@ -81,8 +81,60 @@ class Home extends Base
             ];
         }
         return json(['msg' => 'succeed','code' => 200, 'data' => $data]);
+    }
+    /**
+     * @SWG\Get(
+     *   path="/api/home/leftInfo",
+     *   tags={"Home"},
+     *   summary="左侧信息",
+     *   operationId="updatePetWithForm",
+     *   consumes={"application/x-www-form-urlencoded"},
+     *   produces={"application/json"},
+     *   @SWG\Response(response="201",description="字段不全"),
+     *   security={{
+     *     "petstore_auth": {"write:pets", "read:pets"}
+     *   }}
+     * )
+     */
+    public function leftInfo()
+    {
+        $data = [
+            'game_logo' => '/',
+            'game_name' => '吉林快3',
+            'userbane'  => '傻小子',
+            'balance'   => 888888,
+            'bet'       => [
+                'total'=>5,
+                'data'=>[
+                    ['time'=>date('Y-m-d H:i:s',time()),'content'=>'dfdsf','odds'=>'1.5','money'=>100],
+                    ['time'=>date('Y-m-d H:i:s',time()),'content'=>'dfdsf','odds'=>'1.5','money'=>100],
+                    ['time'=>date('Y-m-d H:i:s',time()),'content'=>'dfdsf','odds'=>'1.5','money'=>100],
+                    ['time'=>date('Y-m-d H:i:s',time()),'content'=>'dfdsf','odds'=>'1.5','money'=>100],
+                    ['time'=>date('Y-m-d H:i:s',time()),'content'=>'dfdsf','odds'=>'1.5','money'=>100],
+                    ['time'=>date('Y-m-d H:i:s',time()),'content'=>'dfdsf','odds'=>'1.5','money'=>100],
+                ],
+            ],
+            'num'       => [
+                'data'=>[
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                    ['date'=>'20191111-11','content'=>110],
+                ]
+            ]
 
+        ];
+
+        return json(['msg' => 'succeed','code' => 200, 'data' => $data]);
 
     }
+
 
 }
