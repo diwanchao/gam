4afceb66bb90c65b011f633b3ccda27d80423d16
xiaochaozi <29862285@qq.com>
2019-03-21 21:01:50 +0800

    头部信息接口

--- application/api/controller/Home.php
+++ application/api/controller/Home.php
@@ -39,5 +39,50 @@ class Home extends Base
 
         return json(['msg' => 'succeed','code' => 200, 'data' => $data]);
     }
+    /**
+     * @SWG\Post(
+     *   path="/api/home/headInfo",
+     *   tags={"Home"},
+     *   summary="头部信息接口",
+     *   operationId="updatePetWithForm",
+     *   consumes={"application/x-www-form-urlencoded"},
+     *   produces={"application/json"},
+     *   @SWG\Parameter(
+     *     name="game_name",
+     *     in="formData",
+     *     description="游戏名",
+     *     required=false,
+     *     type="string",
+     *   ),
+     *   @SWG\Response(response="201",description="字段不全"),
+     *   security={{
+     *     "petstore_auth": {"write:pets", "read:pets"}
+     *   }}
+     * )
+     */
+
+    public function headInfo()
+    {
+        $game_name = $_POST['game_name'];
+
+        $data = [
+            'notice'     => '我是公告',
+            'macau_time' => time(),
+            'game'       => [
+                ['name' => '吉林快3', 'key' => 'jlk3', 'url'=>'/'],
+                ['name' => '时时彩', 'key' => 'ssc', 'url'=>'/'],
+            ],
+        ];
+        if ($game_name) 
+        {
+            $data['subGame'] = [
+                ['name'=>'三军', 'key'=>'ssc','url'=>'/'],
+                ['name'=>'和值', 'key'=>'ssc','url'=>'/'],
+            ];
+        }
+        return json(['msg' => 'succeed','code' => 200, 'data' => $data]);
+
+
+    }
 
 }
