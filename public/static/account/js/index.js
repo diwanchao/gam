4cae78a00e9c162d8ad4ccbe015fcde86f6b244a
hedianjie <1163486362@qq.com>
2019-03-23 13:42:17 +0800

    s

--- public/static/account/js/index.js
+++ public/static/account/js/index.js
@@ -1,33 +1,41 @@
-var game_key = utils.getCookie('game_key');
-var gameList = InfoAll.InitHeader.data.game;
+// var game_key = utils.getCookie('game_key');/\
+
 var tablePage = new Page('#pageInfo', function(index){init();});
 
 var init = function(){
-    if(!game_key){
+    if(!ENV.game_key){
         alert('没有可玩的游戏');
         return;
     }
 
     var p = tablePage.data.index;
     // 用当前第几页 index / game_key ajax请求 获取列表
-    var json = {
-        total: 192, //总共多少条数
-        single: 123, // 单量
-        money: 32, // 金额 
-        school: 43, // 派彩 
-        break: 838, // 退水 
-        get: 23, // 输赢 
-        data: [
-            // 日期	 单量	金额	派彩	退水	输赢
-            {date: '2019-03-10 星期日',
-             single: '0', money: '1.00', school: '0.00', break: '0.00', get: '0.00'},
-            {date: '2019-03-10 星期日', single: '0', money: '1.00', school: '0.00', break: '0.00', get: '0.00'},
-            {date: '2019-03-10 星期日', single: '0', money: '1.00', school: '0.00', break: '0.00', get: '0.00'},
-            {date: '2019-03-10 星期日', single: '0', money: '1.00', school: '0.00', break: '0.00', get: '0.00'},
-        ]
-    }
-    tablePage.init({total: json.total});
-    render(json.data,json.single, json.money, json.school, json.break, json.get);
+    utils.getAjax({
+        url: utils.concatGameKey('/user/settlementList'),
+        type: 'POST',
+        data: {index: p},
+        success: function(json){
+            tablePage.init({total: json.total});
+            render(json.data,json.single, json.money, json.school, json.break, json.get);
+        }
+    })
+    // var json = {
+    //     total: 192, //总共多少条数
+    //     single: 123, // 单量
+    //     money: 32, // 金额 
+    //     school: 43, // 派彩 
+    //     break: 838, // 退水 
+    //     get: 23, // 输赢 
+    //     data: [
+    //         // 日期	 单量	金额	派彩	退水	输赢
+    //         {date: '2019-03-10 星期日',
+    //          single: '0', money: '1.00', school: '0.00', break: '0.00', get: '0.00'},
+    //         {date: '2019-03-10 星期日', single: '0', money: '1.00', school: '0.00', break: '0.00', get: '0.00'},
+    //         {date: '2019-03-10 星期日', single: '0', money: '1.00', school: '0.00', break: '0.00', get: '0.00'},
+    //         {date: '2019-03-10 星期日', single: '0', money: '1.00', school: '0.00', break: '0.00', get: '0.00'},
+    //     ]
+    // }
+    
 }
 
 var render = function(data, single, money, school, b_reak, get){
@@ -44,21 +52,24 @@ var render = function(data, single, money, school, b_reak, get){
 
 $(function(){
 
+    var gameList = InfoAll.InitHeader.data.game;
+
     $('#changeGame').append(function(){
         var html = '';
         if($.isArray(gameList) && gameList.length){
-            if(!game_key){
-                game_key = gameList[0].key;
+            if(!ENV.game_key){
+                ENV.game_key = gameList[0].key;
             }
 
             for(var i = 0; i < gameList.length; i++){
                 html += '<option value="'+ gameList[i].key +'">'+ gameList[i].name +'</option>'
             }
         }
-    }).val(game_key);
+        return html;
+    }).val(ENV.game_key);
 
     $('#changeGame').bind('change', function(){
-        game_key = this.value;
+        ENV.game_key = this.value;
         // tablePage.init({index: 1})
         tablePage.data.index = 1;
         init();
