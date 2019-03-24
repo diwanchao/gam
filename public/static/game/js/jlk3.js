36f11d6e7f256a022b57f87b8384d879f1a9aabc
hedianjie <1163486362@qq.com>
2019-03-24 12:38:42 +0800

    s

--- public/static/game/js/jlk3.js
+++ public/static/game/js/jlk3.js
@@ -1,4 +1,27 @@
-var 
+
+var selectedAry = [];
+
+var quickValue = '';
+
+function setNumber(key){
+    var elementList = $('[data-item='+ key +']');
+    if($.inArray(key, selectedAry) == -1){ // 不存在
+        elementList.addClass('active');
+        if(quickImport){
+            elementList.find('input').val(quickValue);
+        }
+    }
+    else{ // 存在
+        elementList.removeClass('active');
+    }
+}
+
+function setSelectAry(){
+    for(var i = 0; i < selectedAry.length; i++) {
+        var ele = $('[data-item='+ selectedAry[i] +']');
+        ele.find('input').val(quickValue);
+    }
+}
 
 $(function(){
     $('.portlet-body .h-table').find('input[type=text]').bind('keydown', function(e){
@@ -17,4 +40,28 @@ $(function(){
     $('.portlet-body .h-table').find('input[type=text]').bind('click', function(e){
         e.stopPropagation();
     });
+
+
+
+    $('#quickImport').bind('keydown', function(e){
+        if((e.keyCode < 48 || e.keyCode > 57) && e.keyCode != 8 && e.keyCode != 37 && e.keyCode != 39 && e.keyCode != 38 && e.keyCode != 40) {
+            e.preventDefault();
+        }
+    });
+
+    $('#quickImport').bind('blur', function(){
+        if(isNaN(this.value)){
+            alert('请输入纯数字');
+            this.value = '';
+        }
+    });
+
+    $('#quickImport').bind('click', function(e){
+        e.stopPropagation();
+    });
+
+    $('#quickImport').bind('input', function(){
+        quickValue = _this.value;
+        setSelectAry()
+    })
 })
