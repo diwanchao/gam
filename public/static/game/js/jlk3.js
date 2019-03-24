66279fbbb26d7c5c2b85dc33ddd50b1aeb5f329f
hedianjie <1163486362@qq.com>
2019-03-24 11:08:06 +0800

    s

--- public/static/game/js/jlk3.js
+++ public/static/game/js/jlk3.js
@@ -2,12 +2,12 @@
 
 $(function(){
     $('.portlet-body .h-table').find('input').bind('keydown', function(e){
-        if(e.keyCode < 48 && e.keyCode > 57 && e.keyCode != 8 && e.keyCode != 37 && e.keyCode != 39 && e.keyCode != 38 && e.keyCode != 40) {
+        if((e.keyCode < 48 || e.keyCode > 57) && e.keyCode != 8 && e.keyCode != 37 && e.keyCode != 39 && e.keyCode != 38 && e.keyCode != 40) {
             e.preventDefault();
         }
     });
 
-    $('.portlet-body .h-table').find('input').bind('blue', function(){
+    $('.portlet-body .h-table').find('input').bind('blur', function(){
         if(isNaN(parseInt(this.value))){
             alert('请输入纯数字');
             this.value = '';
