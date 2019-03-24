271aae457100bb764c76497ad5d042d3766e9723
hedianjie <1163486362@qq.com>
2019-03-24 11:05:56 +0800

    s

--- public/static/game/js/jlk3.js
+++ public/static/game/js/jlk3.js
@@ -3,12 +3,7 @@
 $(function(){
     $('.portlet-body .h-table').find('input').bind('keydown', function(e){
         if(e.keyCode < 48 && e.keyCode > 57 && e.keyCode != 8 && e.keyCode != 37 && e.keyCode != 39 && e.keyCode != 38 && e.keyCode != 40) {
-            if(e.preventDefault){
-                e.preventDefault();
-            }
-            if(window.event) {
-                window.event.returnValue = false;
-            }
+            e.preventDefault();
         }
     });
 
@@ -20,6 +15,6 @@ $(function(){
     });
 
     $('.portlet-body .h-table').find('input').bind('click', function(e){
-        e.stopPropagationt();
+        e.stopPropagation();
     });
 })
