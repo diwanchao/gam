55338969ccac448d52c3a0ef9b01d854a0e3f552
hedianjie <1163486362@qq.com>
2019-03-24 11:13:41 +0800

    s

--- public/static/game/js/jlk3.js
+++ public/static/game/js/jlk3.js
@@ -1,20 +1,20 @@
-
+var 
 
 $(function(){
-    $('.portlet-body .h-table').find('input').bind('keydown', function(e){
+    $('.portlet-body .h-table').find('input[type=text]').bind('keydown', function(e){
         if((e.keyCode < 48 || e.keyCode > 57) && e.keyCode != 8 && e.keyCode != 37 && e.keyCode != 39 && e.keyCode != 38 && e.keyCode != 40) {
             e.preventDefault();
         }
     });
 
-    $('.portlet-body .h-table').find('input').bind('blur', function(){
+    $('.portlet-body .h-table').find('input[type=text]').bind('blur', function(){
         if(isNaN(this.value)){
             alert('请输入纯数字');
             this.value = '';
         }
     });
 
-    $('.portlet-body .h-table').find('input').bind('click', function(e){
+    $('.portlet-body .h-table').find('input[type=text]').bind('click', function(e){
         e.stopPropagation();
     });
 })
