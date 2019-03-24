e4c52741ebb14ee8fe3457a51532491ac10fa5c6
hedianjie <1163486362@qq.com>
2019-03-24 09:47:18 +0800

    s

--- public/static/js/global.js
+++ public/static/js/global.js
@@ -381,13 +381,13 @@ var ENV = {
 			// 游戏集合
 			game: [
 				{name: '吉林快3', key: 'jlk3', url: '/'},
-				{name: '重庆时时彩', key: 'ccssc', url: '/'}
+				{name: '重庆时时彩', key: 'ssc', url: '/'}
 			],
 			// 游戏详情
-			subGame: [
-				{name: '(三軍、和值、其它)', url: '/'},
-				{name: '(三同号单选、二同号复选、二同号单选、三不同号、二不同号)', key: 'ccssc', url: '/'}
-			],
+			// subGame: [
+			// 	{name: '(三軍、和值、其它)', url: '/'},
+			// 	{name: '(三同号单选、二同号复选、二同号单选、三不同号、二不同号)', key: 'ccssc', url: '/'}
+			// ],
 		};
 
 		this.init();
