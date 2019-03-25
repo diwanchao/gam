1bea269e1d2301c278c47119055db9f3e874bc08
xiaochaozi <29862285@qq.com>
2019-03-25 21:56:01 +0800

    11

--- application/index/controller/Game.php
+++ application/index/controller/Game.php
@@ -11,4 +11,9 @@ class Game extends Controller
     public function jlssc(){
     	return $this->fetch();
     }
+    public function jlk32()
+    {
+    	return $this->fetch('game/jlk3-2');
+    }
+
 }
