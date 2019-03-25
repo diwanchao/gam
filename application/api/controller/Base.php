ab53c22c15dc9f8877e6c62151dd23fda55ae82d
xiaochaozi <29862285@qq.com>
2019-03-25 18:01:35 +0800

    11

--- application/api/controller/Base.php
+++ application/api/controller/Base.php
@@ -2,6 +2,7 @@
 namespace app\api\controller;
 use think\Controller;
 use \think\Session;
+use think\Cache;
 
 class Base extends Controller
 {
@@ -15,6 +16,16 @@ class Base extends Controller
         }else{
         	return json(['msg' => 'fail','code' => 304, 'data' => []]);die();
         }
+        $options = [
+            'type'      => 'redis',//指定类型
+            'password'  => '',
+            'prefix'    => '',
+            'host'      => '127.0.0.1',
+            'port'      => 6379,
+            'timeout'   => 0,
+        ];
+        //Cache::init($options);//初始化
     }
 
 }
+         
