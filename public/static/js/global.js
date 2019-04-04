20e0699e6c40414bd45265e8f14c68fb9ec91db9
hedianjie <1163486362@qq.com>
2019-04-04 09:05:23 +0800

    s

--- public/static/js/global.js
+++ public/static/js/global.js
@@ -41,7 +41,7 @@ var utils = {
 		var Days = 1;
 		var exp = new Date();
 		exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
-		document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString() + 'path=/';
+		document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString() + ';path=/';
 	},
 
 	/**
