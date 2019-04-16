695c8f9d8a03d6c6aa9740b22186966346babc86
xiaochaozi <29862285@qq.com>
2019-04-16 20:59:54 +0800

    1

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -142,8 +142,10 @@ class User extends Base
                 $sum_school += $data[$value]['school'];
                 $sum_break  += $data[$value]['break'];
                 $sum_get    += $data[$value]['get'];
-
                 $new_data[$key] = $data[$value];
+                $new_data[$key]['school']   = sprintf("%.2f",$new_data[$key]['school']);
+                $new_data[$key]['break']    = sprintf("%.2f",$new_data[$key]['break']);
+                $new_data[$key]['get']      = sprintf("%.2f",$new_data[$key]['get']);
             }else{
                 $new_data[$key] = ['date'=>$value,'single'=>0,'money'=>0,'school'=>0,'break'=>0,'get'=>0];
             }
